1248153fc7fe6a9589cdae173c50880959826daa
Safdar Nazir <safdarnazir14@gmail.com>
2021-04-07 18:14:19 +0500

    Added constants.dart

--- lib/frontend/screens/login/loginScreen.dart
+++ lib/frontend/screens/login/loginScreen.dart
@@ -1,5 +1,7 @@
+import 'package:app_with_flutter2/config/constants.dart';
 import 'package:app_with_flutter2/config/sizeConfig.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -11,17 +13,57 @@ class LoginScreen extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: EdgeInsets.symmetric(
-              horizontal: getScreenWidth(60),
+              horizontal: getScreenWidth(40),
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Icon(
+                  FontAwesomeIcons.clock,
+                  size: getScreenHeight(90),
+                  color: kPrimaryColor,
+                ),
+                SizedBox(
+                  height: getScreenHeight(20),
+                ),
                 Text(
                   'Quizaholic',
                   style: TextStyle(
                     fontSize: getScreenHeight(28),
                     fontWeight: FontWeight.bold,
                     letterSpacing: 1.3,
+                    color: kPrimaryColor,
+                  ),
+                ),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          isDense: true,
+                          hintText: 'Email',
+                          hintStyle: TextStyle(
+                            height: 1,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: kPrimaryColor,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: getScreenWidth(14),
+                            vertical: getScreenHeight(16),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: getScreenHeight(20),
+                      ),
+                    ],
                   ),
                 ),
                 Spacer(),
